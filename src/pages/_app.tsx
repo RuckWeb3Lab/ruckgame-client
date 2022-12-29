917058c7ec994eef045316415aada34ff7cb0ab7
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-12-29 09:16:18 +0900

    fix: add page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    type: "dark", // it could be "light" or "dark"
+  })
+
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+}
