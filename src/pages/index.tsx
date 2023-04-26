7990f205bec788f3a46746a4182b9b256bf253d1
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2023-04-26 16:38:51 +0900

    fix: リファクタ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { useRouter } from 'next/router'
 import { SERVICE_NAME } from '@/globals/constants'
-// Mui
 import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
-// Component
 import GuestLayout from '@/components/layout/GuestLayout'
-// Type
 import type { NextPageWithLayout } from '@/pages/_app'
 
 const IndexPage: NextPageWithLayout = () => {
